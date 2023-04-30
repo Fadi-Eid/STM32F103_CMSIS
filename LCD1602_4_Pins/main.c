06b42d1580dbc2bd778bc5e53ae4c02a8aea955e
Fadi EID <113466842+Fadi-Eid@users.noreply.github.com>
2023-04-30 20:23:08 +0300

    Add files via upload

---
+++ LCD1602_4_Pins/main.c
@@ -0,0 +1,26 @@
+#include "gpio.h"
+#include "systick_time.h"
+#include "lcd_1602.h"
+/*
+Pin setup : 4 bits data communication
+
+PA8 -> RS
+PA9 -> RW
+PA10-> E
+
+PA4 -> DB4
+PA5 -> DB5
+PA6 -> DB6
+PA7 -> DB7
+*/
+
+// update init for 4 bit data transfer 
+
+int main(void)
+{
+		systick_init();// initialize the delay function (Must initialize)
+		lcd_init();
+		lcd_msg(1, 0,"Welcome to the");
+		lcd_msg(2, 6,"WeeW");
+	while(1);
+}
