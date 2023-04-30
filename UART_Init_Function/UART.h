07ea6f859630d4f9c076f3e7628936bd14fff79f
Fadi EID <113466842+Fadi-Eid@users.noreply.github.com>
2023-04-30 02:29:53 -0700

    Update UART.h

--- UART_Init_Function/UART.h
+++ UART_Init_Function/UART.h
@@ -7,5 +7,6 @@ void UART_Init(uint8_t USART, uint32_t BaudRate);
 uint32_t BaudRate_Calc(uint32_t BaudRate, uint32_t freq);
 void transmit_char(uint8_t USART, uint8_t data);
 void transmit_string(uint8_t USART, uint8_t* data, uint16_t length);
+uint8_t receive_char(uint8_t USART);
 
 #endif
